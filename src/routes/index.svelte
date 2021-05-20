d1667ed1847de42154631b8a3b69cbbf8e4915d1
Brett Wilcox <brett@scorpion.io>
2021-05-19 20:19:05 -0500

    link added. much wow.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,11 @@
-<script>
-	let name = 'User';
-
-</script>
-
 <svelte:head>
 	<title>SIM</title>
 </svelte:head>
 
-<h1>Hello {name}!</h1>
-
 <div class="bg-white dark:bg-gray-800">
 	<p class="text-gray-600 dark:text-gray-300">Coming soon.</p>
-	<a href="https://github.com/sim-shell/sim">GitHub</a>
+</div>
+
+<div>
+	<p><a href="https://github.com/sim-shell/sim">GitHub</a></p>
 </div>
